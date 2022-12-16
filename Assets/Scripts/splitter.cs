b1fd493cd224efacf499ba86b95b307dd8ab5bd3
elementalearth123 <hanchoof@outlook.com>
2022-12-16 13:41:33 -0800

    Update splitter.cs

--- Assets/Scripts/splitter.cs
+++ Assets/Scripts/splitter.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class splitter : MonoBehaviour
 {
     public GameObject splitChild;
+    public Health hpScript;
 
+    private void Start()
+    {
+        hpScript = gameObject.GetComponent<Health>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if(gameObject == null)
+        if(hpScript.currentHealth == 1)
         {
             Instantiate(splitChild, transform.position, transform.rotation);
             Instantiate(splitChild, transform.position, transform.rotation);
+            hpScript.currentHealth = 0;
         }
     }
 }
