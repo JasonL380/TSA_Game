b3c7a3542d9cadb531a347a09c4a882ee8f0900b
Zak F Baydass <80441141+TaloozaGames@users.noreply.github.com>
2022-12-16 19:31:28 -0800

    simple reworks
    blargh

--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -2,31 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class CursorMovement : MonoBehaviour
 {
     Vector2 positionValue;
     public bool isGamePad;
     public GameObject player;
+    public Rigidbody2D rb2d;
+    public new Camera camera;
+    public int speed;
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        player.transform.position = positionValue;
+
+       // if (Gamepad.current != null)
+       // {
+        //    rb2d.velocity = positionValue * speed;
+        //}
     }
 
-   public void OnCursor(InputAction.CallbackContext value)
+    public void OnCursor(InputAction.CallbackContext value)
     {
+            positionValue = value.ReadValue<Vector2>();
+    }
 
+    public void OnGamePadCursor(InputAction.CallbackContext value)
+    {
         positionValue = value.ReadValue<Vector2>();
+        rb2d.velocity = positionValue * speed;
     }
 
     void OnClick()
     {
-
+        
     }
 }
