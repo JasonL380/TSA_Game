b288511ae22c10f5354382e27a0b9ef8fb420575
TaloozaGames <80441141+TaloozaGames@users.noreply.github.com>
2023-01-20 13:56:22 -0800

    working on Tower Select

---
+++ Assets/Scripts/TowerMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TowerMenu : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject Cursor;
+
+    public bool IsOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Menu = GetComponent<GameObject>();
+        Menu.SetActive(false);
+        IsOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnOpenTowerMenu()
+    {
+        if(IsOpen)
+        {
+            CloseMenu();
+        }
+        else if(IsOpen == false)
+        {
+            OpenMenu();
+        }
+    }
+    void OpenMenu()
+    {
+        Cursor.SetActive(false);
+        Menu.SetActive(true);
+    }
+    void CloseMenu()
+    {
+        Cursor.SetActive(true);
+        Menu.SetActive(false);
+    }
+}
--- Assets/Scripts/WaveSystem.cs
+++ Assets/Scripts/WaveSystem.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.VirtualTexturing;
 
 public class WaveSystem : MonoBehaviour
 {
