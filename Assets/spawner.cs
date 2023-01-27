beae7f270d62352f1b937b706a84246b01798b25
elementalearth123 <hanchoof@outlook.com>
2023-01-27 14:28:40 -0800

    A bunch of small changes
    Enemies have tags and go to layers, turrets have tags, health has more stuff

---
+++ Assets/spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject spawn;
+    public int reqGen;
+    public int reqSpec;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
